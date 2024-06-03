d0512b5a8622c275e6e9ab48d1d05dbc8cf8bda4
KevinMurcia636 <146030036+BlacKomm636@users.noreply.github.com>
2024-06-03 10:14:32 -0500

    Add files via upload

---
+++ ProyectoDOO-feature-backend/src/main/java/com/uco/pch/PchApplication.java
@@ -0,0 +1,12 @@
+package com.uco.pch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PchApplication.class, args);
+	}
+}
